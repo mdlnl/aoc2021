6794aa3b24b698ec418773a9903f1897e944f895
Madeline Lein <madeline.lein@gmail.com>
2021-12-07 21:42:55 -0800

    last pre-permutation code

--- day08/solution.py
+++ day08/solution.py
@@ -1,9 +1,14 @@
 import day08.inputs as inputs
 
+def strsort(s):
+    x = list(s)
+    x.sort()
+    return ''.join(x)
+
 def parse(entry):
     [signal_strs, digit_strs] = entry.split(' | ')
-    signals = signal_strs.split(' ')
-    digits = digit_strs.split(' ')
+    signals = [strsort(s) for s in signal_strs.split(' ')]
+    digits = [strsort(d) for d in digit_strs.split(' ')]
     return (signals, digits)
 
 def parse_all(entries):
@@ -26,7 +31,18 @@ def part1(inp):
     return sum(count1478(digits) for digits in digitses)
 
 def part2(inp):
-    return None
+    return sum(output(signals, digits) for (signals, digits) in inp)
+
+def output(signals, digits):
+    signal_map = {
+        next(s for s in signals if len(s) == 2): 1,
+        next(s for s in signals if len(s) == 4): 4,
+        next(s for s in signals if len(s) == 3): 7,
+        next(s for s in signals if len(s) == 7): 8,
+    }
+    return 0
 
 assert part1(parse_all(inputs.sample)) == 26
-print(part1(parse_all(inputs.full)))
+assert part1(parse_all(inputs.full)) == 344
+
+print(part2(parse_all(inputs.sample)))
