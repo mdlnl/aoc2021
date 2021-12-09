ca44424705f5c2a4d12c4b672a6934e9a3270892
Madeline Lein <madeline.lein@gmail.com>
2021-12-08 22:11:36 -0800

    finished day9 part2

--- day09/solution.py
+++ day09/solution.py
@@ -37,5 +37,31 @@ def part1(height_map):
     return sum(height_map[i][j] + 1 for (i, j) in find_local_minima(height_map))
 
 assert part1(parse(inputs.sample)) == 15
+assert part1(parse(inputs.full)) == 425
 
-print(part1(parse(inputs.full)))
+def find_basin(height_map, i0, j0):
+    queue = [(i0, j0)]
+    m = len(height_map)
+    n = len(height_map[0])
+    basin = set()
+    while queue:
+        (i, j) = queue.pop(0)
+        if height_map[i][j] < 9:
+            basin.add((i, j))
+            for (ni, nj) in neighbors(m, n, i, j):
+                if (ni, nj) not in basin and (ni, nj) not in queue:
+                    queue.append((ni, nj))
+    return basin
+
+def part2(height_map):
+    basins = [
+        find_basin(height_map, i, j)
+        for (i, j) in find_local_minima(height_map)
+    ]
+    basin_sizes = [len(b) for b in basins]
+    basin_sizes.sort()
+    return basin_sizes[-1] * basin_sizes[-2] * basin_sizes[-3]
+
+assert part2(parse(inputs.sample)) == 1134
+
+print(part2(parse(inputs.full)))
