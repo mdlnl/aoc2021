2823ed0fba3e6334dadaaa7bc260374d227d4be8
Madeline Lein <61875345+mdlnl@users.noreply.github.com>
2021-12-09 22:58:56 +0000

    Simplifications. day08.solve doesn't need to explicitly return a lambda. Remove unused imports.

--- day08/solution.hs
+++ day08/solution.hs
@@ -1,6 +1,4 @@
-import Data.List (intercalate)
 import Data.Set (Set, size, fromList, isSubsetOf, singleton, difference, (\\), elems)
-import Debug.Trace
 import System.IO
 
 isPrefix [] _ = True
@@ -36,33 +34,32 @@ setFilter :: (Ord a) => (a -> Bool) -> Set a -> Set a
 setFilter f s = fromList $ filter f $ elems s
 
 solve :: [String] -> String -> Char
-solve signalStrings = (\sigString -> case () of
-                           _ | sigs == zero  -> '0'
-                             | sigs == one   -> '1'
-                             | sigs == two   -> '2'
-                             | sigs == three -> '3'
-                             | sigs == four  -> '4'
-                             | sigs == five  -> '5'
-                             | sigs == six   -> '6'
-                             | sigs == seven -> '7'
-                             | sigs == eight -> '8'
-                             | sigs == nine  -> '9'
-                             | otherwise     -> error "unknown signal"
-                             where sigs = fromList sigString
-        )
-        where signals = map fromList signalStrings
-              one = uniqueSize 2 signals
-              four = uniqueSize 4 signals
-              seven = uniqueSize 3 signals
-              eight = uniqueSize 7 signals
-              sixes = fromList $ filter (\e -> (size e) == 6) signals
-              nine = only $ filter (isSubsetOf four) $ elems sixes
-              zero = only $ filter (isSubsetOf one) $ elems $ sixes \\ (singleton nine)
-              six = only $ elems $ difference sixes $ fromList [zero, nine]
-              fives = fromList $ filter (\e -> (size e) == 5) $ signals
-              three = only $ filter (isSubsetOf seven) $ elems fives
-              five = only $ filter (flip isSubsetOf six) $ elems fives
-              two = only $ elems $ difference fives $ fromList [three, five]
+solve signalStrings digitString
+    | digit == zero  = '0'
+    | digit == one   = '1'
+    | digit == two   = '2'
+    | digit == three = '3'
+    | digit == four  = '4'
+    | digit == five  = '5'
+    | digit == six   = '6'
+    | digit == seven = '7'
+    | digit == eight = '8'
+    | digit == nine  = '9'
+    | otherwise      = error "unknown signal"
+    where signals = map fromList signalStrings
+          one = uniqueSize 2 signals
+          four = uniqueSize 4 signals
+          seven = uniqueSize 3 signals
+          eight = uniqueSize 7 signals
+          sixes = fromList $ filter (\e -> (size e) == 6) signals
+          nine = only $ filter (isSubsetOf four) $ elems sixes
+          zero = only $ filter (isSubsetOf one) $ elems $ sixes \\ (singleton nine)
+          six = only $ elems $ difference sixes $ fromList [zero, nine]
+          fives = fromList $ filter (\e -> (size e) == 5) $ signals
+          three = only $ filter (isSubsetOf seven) $ elems fives
+          five = only $ filter (flip isSubsetOf six) $ elems fives
+          two = only $ elems $ difference fives $ fromList [three, five]
+          digit = fromList digitString
 
 part2 (Entry signals digits) = read $ map solution digits :: Integer
     where solution = solve signals
