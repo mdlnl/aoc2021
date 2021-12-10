8b67da43d53dcfb1df76e8dc8da41fa8951cbabe
Madeline Lein <madeline.lein@gmail.com>
2021-12-09 21:35:18 -0800

    10.2 autocomplete

--- day10/solution.py
+++ day10/solution.py
@@ -1,6 +1,6 @@
 import inputs
 
-lefts = '([{<'
+lefts = { '(':')', '[':']', '{':'}', '<':'>' }
 rights = { ')':'(', ']':'[', '}':'{', '>':'<' }
 
 def parse(str):
@@ -12,20 +12,34 @@ def parse(str):
             if stack[-1] == rights[str[i]]:
                 stack.pop()
             else:
-                return i
-    return None
+                return i, stack
+    return None, stack
 
 errorScores = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
 
 def errorScore(line):
-    p = parse(line)
+    p, _ = parse(line)
     return errorScores[line[p]] if p else 0
 
 def part1(lines):
     return sum(errorScore(line) for line in lines)
 
-def autocompleteScore(line):
-    p = parse(line)
+autocompleteScores = { ')': 1, ']': 2, '}': 3, '>': 4 }
+
+def autocomplete(line):
+    p, stack = parse(line)
+    if not p:
+        a = []
+        score = 0
+        #print(stack)
+        while stack:
+            r = lefts[stack.pop()]
+            score = score * 5 + autocompleteScores[r]
+            a.append(r)
+        return a, score
 
 assert part1(inputs.sample) == 26397
 assert part1(inputs.full) == 367227
+
+_, a = autocomplete('[({(<(())[]>[[{[]{<()<>>')
+assert a == 288957
