8b5425393bffcbb134d91a3b8bc7cc308b56622a
Madeline Lein <madeline.lein@gmail.com>
2021-12-09 22:49:11 -0800

    broke part1 answer but code is done

--- day10/solution.hs
+++ day10/solution.hs
@@ -1,4 +1,5 @@
 import Data.Maybe
+--import Data.Sort
 import Debug.Trace
 import Split
 import System.IO
@@ -25,6 +26,9 @@ data Result = Error Int Char String | Incomplete String | Complete deriving (Sho
 isError (Error _ _ _) = True
 isError _ = False
 
+isIncomplete (Incomplete _) = True
+isIncomplete _ = False
+
 parse line = parseAux line 0 []
 parseAux [] pos [] = Complete
 parseAux [] pos stack@(_:_) = Incomplete stack
@@ -44,7 +48,27 @@ errorScore _ = 0
 
 part1 lines = sum [errorScore $ parse line | line <- lines]
 
+autocompleteScore '(' = 1
+autocompleteScore '[' = 2
+autocompleteScore '{' = 3
+autocompleteScore '<' = 4
+autocompleteScore c = error $ "unexpected stack item " ++ [c]
+
+totalAutocompleteScore (Incomplete stack) = foldl acc 0 stack
+    where acc score top = score * 5 + (autocompleteScore top)
+
+quicksort [] = []
+quicksort (x:xs) = left ++ (x : right)
+    where left = filter (<x) xs
+          right = filter (>=x) xs
+
+part2 lines = scores !! m
+    where results = map parse lines
+          scores = quicksort $ map totalAutocompleteScore $ filter isIncomplete results
+          m = (length scores) `div` 2
+
 main = do
-    input <- readFile "sample.txt"
+    input <- readFile "full.txt"
     let lines = nlsplit input
     putStrLn $ "part1 " ++ (show $ part1 lines)
+    putStrLn $ "part2 " ++ (show $ part2 lines)
