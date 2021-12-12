3ba6f44ff40e1109198f610d9efe7d7962893600
Madeline Lein <madeline.lein@gmail.com>
2021-12-12 15:22:05 -0800

    12.2 py sample

--- day12/solution.py
+++ day12/solution.py
@@ -9,25 +9,56 @@ def parseGraph(lines):
         graph[endpoints[1].strip()].add(endpoints[0])
     return graph
 
-def traverse(graph, path=['start'], smalls=defaultdict(lambda: 0), smallLimit=1):
+def traverseNoSecondSmall(graph, path=['start'], smalls=set(), debug=False):
     ''' traverse(graph, prefix): returns all paths that start with prefix and end at 'end' '''
-    #print(f'traversing from {path}')
+    if debug:
+        print(f'traversing from {",".join(path)}')
     if path[-1] == 'end':
         return [path]
     completePaths = []
     for outEdge in graph[path[-1]] - {'start'}:
-        if smalls[outEdge] < smallLimit:
-            newSmalls = smalls.copy()
-            if outEdge.islower():
-                newSmalls[outEdge] = newSmalls[outEdge] + 1
-            completePaths = completePaths + traverse(graph, path + [outEdge], newSmalls)
+        if outEdge in smalls:
+            continue
+        newSmalls = smalls.union({outEdge}) if outEdge.islower() else smalls
+        completePaths = completePaths + traverseNoSecondSmall(
+            graph, path + [outEdge], newSmalls, debug)
     return completePaths
 
 def part1(filename):
     f = open(filename, 'r')
     lines = f.readlines()
     f.close()
-    return len(traverse(parseGraph(lines)))
+    return len(traverseNoSecondSmall(parseGraph(lines)))
 
 assert part1('sample.txt') == 10
 assert part1('full.txt') == 4659
+
+def traverseOneSecondSmall(graph, path=['start'], smalls=set(), secondSmall=None, debug=False):
+    ''' traverse(graph, prefix): returns all paths that start with prefix and end at 'end' '''
+    if debug:
+        print(f'{",".join(path)}. Can reach {" ".join(graph[path[-1]])}')
+    if path[-1] == 'end':
+        return [path]
+    completePaths = []
+    for outEdge in graph[path[-1]] - {'start'}:
+        newSecondSmall = secondSmall
+        if outEdge in smalls:
+            if secondSmall:
+                if debug:
+                    print(f'skipping {outEdge}, already double-visited {secondSmall}')
+                continue
+            newSecondSmall = outEdge
+            if debug:
+                print(f'allowed double-visit of {outEdge}')
+        newSmalls = smalls.union({outEdge}) if outEdge.islower() else smalls
+        completePaths = completePaths + traverseOneSecondSmall(
+            graph, path + [outEdge], newSmalls, newSecondSmall, debug)
+    return completePaths
+
+def part2(filename):
+    f = open(filename, 'r')
+    lines = f.readlines()
+    f.close()
+    return len(traverseOneSecondSmall(parseGraph(lines)))
+
+assert part2('sample.txt') == 36
