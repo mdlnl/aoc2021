9f4e9d24e341150f2885e5f43166061bf9a7e7b4
Madeline Lein <61875345+mdlnl@users.noreply.github.com>
2021-12-13 22:50:40 +0000

    Rename DoublyLinkedGrid to LinkedGrid

--- LinkedGrid.hs
+++ LinkedGrid.hs
@@ -1,27 +1,27 @@
 module LinkedGrid where
 
--- DLG rowsAboveReversed rowsAtOrBelow columnIndex
-data DoublyLinkedGrid a = DLG [a] [a] Int
+-- LG rowsAboveReversed rowsAtOrBelow columnIndex
+data LinkedGrid a = LG [a] [a] Int
 
---instance Show a => Show (DoublyLinkedGrid a) where
-  --show (DLG aboves atOrBelows columnIndex) = showDlgColMarker
+--instance Show a => Show (LinkedGrid a) where
+  --show (LG aboves atOrBelows columnIndex) = showDlgColMarker
 
-fromListOfLists outerList = DLG [] outerList 0
+fromListOfLists outerList = LG [] outerList 0
 
-rows (DLG aboves atOrBelows _) = length aboves + length atOrBelows
+rows (LG aboves atOrBelows _) = length aboves + length atOrBelows
 
-columns (DLG _ (at:_) _) = length at
+columns (LG _ (at:_) _) = length at
 
-up (DLG [] _ _) = error "Already at the top."
-up (DLG (above:aboves) atOrBelows columnIndex) = DLG aboves (above:atOrBelows) columnIndex
+up (LG [] _ _) = error "Already at the top."
+up (LG (above:aboves) atOrBelows columnIndex) = LG aboves (above:atOrBelows) columnIndex
 
-down (DLG _ [] _) = error "Already at the bottom."
-down (DLG aboves (at:belows) columnIndex) = DLG (at:aboves) belows columnIndex
+down (LG _ [] _) = error "Already at the bottom."
+down (LG aboves (at:belows) columnIndex) = LG (at:aboves) belows columnIndex
 
-left (DLG _ _ 0) = error "Already at the left edge."
-left (DLG aboves atOrBelows columnIndex) = DLG aboves atOrBelows (columnIndex - 1)
+left (LG _ _ 0) = error "Already at the left edge."
+left (LG aboves atOrBelows columnIndex) = LG aboves atOrBelows (columnIndex - 1)
 
-right (DLG _ [] _) = error "Fell off the world."
-right (DLG aboves (at:belows) columnIndex)
-    | length at > columnIndex + 1 = DLG aboves (at:belows) (columnIndex + 1)
+right (LG _ [] _) = error "Fell off the world."
+right (LG aboves (at:belows) columnIndex)
+    | length at > columnIndex + 1 = LG aboves (at:belows) (columnIndex + 1)
     | otherwise                   = error "Already at the right edge."
