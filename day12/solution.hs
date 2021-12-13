c2b201d1c3091f412ec132b9920a821d27aa1ca0
Madeline Lein <madeline.lein@gmail.com>
2021-12-12 16:51:02 -0800

    12.2 haskell

--- day12/solution.hs
+++ day12/solution.hs
@@ -1,34 +1,64 @@
 import qualified Data.MultiMap as MM
 import Data.List
+import Data.Maybe
 import Debug.Trace
 import Split
 import Text.Printf
 
-parseEdge line = (from, to)
-    where [from, to] = split "-" line
+type Graph = MM.MultiMap String String
+type Node = String
+type Path = [Node]
 
-parseGraph :: [String] -> MM.MultiMap String String
+parseGraph :: [String] -> Graph
 parseGraph [] = MM.empty
 parseGraph (line:lines) = MM.insert from to $ MM.insert to from $ parseGraph lines
-    where (from, to) = parseEdge line
+    where [from, to] = split "-" line
 
-maybeAddSmall :: String -> [String] -> [String]
+maybeAddSmall :: Node -> [Node] -> [Node]
 maybeAddSmall cave smalls
     | all (\c -> elem c ['a'..'z']) cave = cave : smalls
     | otherwise = smalls
 
-traverseNoSecondSmall :: MM.MultiMap String String -> [String] -> [String] -> [[String]]
+outEdges :: Graph -> Node -> [Node]
+outEdges graph v = [oe | oe <- MM.lookup v graph, oe /= "start"]
+
+-- traverseNoSecondSmall graph pathPrefix smalls -> [path]
+traverseNoSecondSmall :: Graph -> Path -> [Node] -> [Path]
 traverseNoSecondSmall _ path@("end":_) _ = [reverse path]
 traverseNoSecondSmall graph (h:t) smalls
     | elem h smalls = []
-    | otherwise = foldr (++) [] outPaths
-    where outEdges = [oe | oe <- MM.lookup h graph, oe /= "start"]
-          newSmalls = maybeAddSmall h smalls
-          outPaths = [traverseNoSecondSmall graph (oe : h : t) newSmalls | oe <- outEdges]
+    | otherwise     = foldr (++) [] outPaths
+    where newSmalls = maybeAddSmall h smalls
+          outPaths = [traverseNoSecondSmall graph (oe : h : t) newSmalls | oe <- outEdges graph h]
+
+part1 :: String -> Int
+part1 input = length $ traverseNoSecondSmall graph ["start"] []
+    where graph = parseGraph $ split "\n" input
+
+-- traverseOneSecondSmall graph pathPrefix smalls secondSmall -> [path]
+traverseOneSecondSmall :: Graph -> Path -> [Node] -> Maybe Node -> [Path]
+traverseOneSecondSmall _ path@("end":_) _ _ = [reverse path]
+traverseOneSecondSmall graph (h:t) smalls ss@(Just _)
+    | elem h smalls = []
+    | otherwise     = foldr (++) [] outPaths
+    where newSmalls = maybeAddSmall h smalls
+          outPaths = [
+            traverseOneSecondSmall graph (oe : h : t) newSmalls ss | oe <- outEdges graph h]
+traverseOneSecondSmall graph (h:t) smalls Nothing
+    | elem h smalls = foldr (++) [] [
+            traverseOneSecondSmall graph (oe : h : t) newSmalls (Just h) | oe <- outEdges graph h]
+    | otherwise     = foldr (++) [] [
+            traverseOneSecondSmall graph (oe : h : t) newSmalls Nothing | oe <- outEdges graph h]
+    where newSmalls = maybeAddSmall h smalls
+
+part2 :: String -> Int
+part2 input = length $ traverseOneSecondSmall graph ["start"] [] Nothing
+    where graph = parseGraph $ split "\n" input
 
 main = do
-    input <- readFile "full.txt"
-    let lines = split "\n" input
-    let graph = parseGraph lines
-    let paths = traverseNoSecondSmall graph ["start"] []
-    putStrLn $ show $ length paths
+    sample <- readFile "sample.txt"
+    full <- readFile "full.txt"
+    putStrLn $ "part1(sample): " ++ (show $ part1 sample)
+    putStrLn $ "part1(full): " ++ (show $ part1 full)
+    putStrLn $ "part2(sample): " ++ (show $ part2 sample)
+    putStrLn $ "part2(full): " ++ (show $ part2 full)
