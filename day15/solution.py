9cb4deed243509a84552d70501c315e4eb90081d
Madeline Lein <madeline.lein@gmail.com>
2021-12-14 22:10:45 -0800

    replace real grid with virtual nonrepeating grid in part 1

--- day15/solution.py
+++ day15/solution.py
@@ -5,22 +5,20 @@ def parse(filename):
     return [ [int(c) for c in line if c.isdigit()] for line in lines ]
 
 # Assume can only go down or right
-def shortest_path_dr(risk):
-    rows, cols = len(risk), len(risk[0])
-    assert rows == cols
+def shortest_path_dr(risk, rows, cols):
     best = [[0]] # no cost to get to top left square since we start there
     while len(best) < rows or len(best[-1]) < cols:
         # expand frontier to the right
         f = len(best[0])
         for i in range(len(best)): # exclude lower-right corner
-            fromLeft  = best[i    ][-1] + risk[i][f]
-            fromAbove = best[i - 1][ f] + risk[i][f] if i > 0 else fromLeft + 1
+            fromLeft  = best[i    ][-1] + risk(i, f)
+            fromAbove = best[i - 1][ f] + risk(i, f) if i > 0 else fromLeft + 1
             best[i].append(min(fromAbove, fromLeft))
         # expand frontier down
         frontier = []
         for j in range(f+1): # include lower-right corner
-            fromAbove = best[-1][ j] + risk[f][j]
-            fromLeft  = frontier[-1] + risk[f][j] if j > 0 else fromAbove + 1
+            fromAbove = best[-1][ j] + risk(f, j)
+            fromLeft  = frontier[-1] + risk(f, j) if j > 0 else fromAbove + 1
             frontier.append(min(fromAbove, fromLeft))
         best.append(frontier)
     return best
@@ -28,8 +26,13 @@ def shortest_path_dr(risk):
 def show_grid(grid):
     return '\n'.join(' '.join(f'{c if c else " ":2}') for row in grid for c in row)
 
-assert shortest_path_dr(parse('sample.txt'))[-1][-1] == 40
-print(shortest_path_dr(parse('full.txt'))[-1][-1])
+def part1(filename):
+    grid = parse(filename)
+    g = lambda i, j: grid[i][j]
+    return shortest_path_dr(g, len(grid), len(grid))
+
+assert part1('sample.txt')[-1][-1] == 40
+assert part1('full.txt')[-1][-1] == 824
 
 # ___f?????
 # ___f?????
