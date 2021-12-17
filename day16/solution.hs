e4bbdb9637afe13382c623a2b2f553e98139d683
Madeline Lein <madeline.lein@gmail.com>
2021-12-16 20:38:43 -0800

    fixed some double-parsing bugs to get example2 working

--- day16/solution.hs
+++ day16/solution.hs
@@ -1,6 +1,7 @@
 import Data.List (intercalate)
 import qualified Data.Map as Map (fromList, lookup)
 import Data.Maybe (Maybe(Just))
+import Debug.Trace
 import Numeric
 
 -----------------------------------------
@@ -18,7 +19,7 @@ data Packet = Literal Header Int Int
             | Operator Header [Packet]
 
 instance Show Packet where
-    show (Literal (Hdr version _) _ value)       = (show version) ++ "=" ++ (show value)
+    show (Literal (Hdr version _) numGroups value)       = (show version) ++ "=" ++ (show value) ++ "(" ++ show numGroups ++ ")"
     show (Operator (Hdr version _) subpackets) = (show version) ++ "{" ++ (intercalate ", " $ map show subpackets) ++ "}"
 
 packetLength :: Packet -> Int
@@ -77,31 +78,36 @@ hexToBits (h:hs) = (bitsFromString firstBits) ++ moreBits
 data ParseResult a = PR a BitString | EndOfInput deriving Show
 type Parser a = BitString -> ParseResult a
 
+ptrace f bs = trace ("TRACE " ++ f ++ " [" ++ bitsToString bs ++ "]")
+
 parsePacket :: Parser Packet
-parsePacket [] = EndOfInput
+parsePacket [] = error "Expected packet"
 parsePacket bs
-    | typeId == 4 = parseLiteral header bs
-    | otherwise   = parseOperator header bs
-    where PR header hRemainder = parseHeader bs
+    | typeId == 4 = parseLiteral header hRemainder
+    | otherwise   = parseOperator header hRemainder
+    where PR header hRemainder = --ptrace "packet" bs $
+                                 parseHeader bs
           Hdr _ typeId = header
 
 parseHeader :: Parser Header  
-parseHeader [] = EndOfInput
+parseHeader [] = error "Expected header"
 parseHeader bs = PR (Hdr v t) tr
     where PR v vr = parseVersion bs
           PR t tr = parseTypeId vr
 
 parseLiteral :: Header -> Parser Packet
+parseLiteral _ [] = error "Expected literal packet"
 parseLiteral h bs = PR packet gRemainder
-    where PR header hRemainder = parseHeader bs
-          PR groups gRemainder = parseGroups hRemainder
+    where PR groups gRemainder = parseGroups bs
           value = bits $ concat groups
           packet = Literal h (length groups) value
 
 parseOperator :: Header -> Parser Packet
+parseOperator _ [] = error "Expected operator packet"
 parseOperator h (Zero:bs) = PR packet spRemainder
     where PR spLength lRemainder = parseInt 15 bs
-          PR subpackets spRemainder = parseSubpacketsByLength spLength lRemainder
+          PR subpackets spRemainder = --trace ("LENGTH=" ++ bitsToString (take 15 bs)) $
+                                      parseSubpacketsByLength spLength lRemainder
           packet = Operator h subpackets
 parseOperator h (One:bs) = PR packet spRemainder
     where PR spCount lRemainder = parseInt 11 bs
@@ -109,19 +115,24 @@ parseOperator h (One:bs) = PR packet spRemainder
           packet = Operator h subpackets
 
 parseSubpacketsByCount :: Int -> Parser [Packet]
+parseSubpacketsByCount n [] = error $ "Expected " ++ show n ++ " subpackets"
 parseSubpacketsByCount 0 bs = PR [] bs
 parseSubpacketsByCount n bs = PR (first:more) mspRemainder
     where PR first fRemainder = parsePacket bs
           PR more mspRemainder = parseSubpacketsByCount (n-1) fRemainder
 
 parseSubpacketsByLength :: Int -> Parser [Packet]
+parseSubpacketsByLength n [] = error $ "Expected " ++ show n ++ " bits"
 parseSubpacketsByLength 0 bs = PR [] bs
 parseSubpacketsByLength len bs = PR (first:more) mspRemainder
-    where PR first fRemainder = parsePacket bs
+    where PR first fRemainder = --ptrace ("parseSubpacketsByLength" ++ show len) bs $
+                                parsePacket bs
           remainingLength = len - (packetLength first)
-          PR more mspRemainder = parseSubpacketsByLength remainingLength fRemainder
+          PR more mspRemainder = --trace ("First=[" ++ show first ++ "], |First|=" ++ show (packetLength first) ++ ", Remaining " ++ show remainingLength ++ " [" ++ bitsToString fRemainder ++ "]") $
+                                 parseSubpacketsByLength remainingLength fRemainder
 
 parseInt :: Int -> Parser Int
+parseInt n [] = error $ "Expected " ++ show n ++ " bits forming an int."
 parseInt n bs = PR (bits (take n bs)) (drop n bs)
 parseVersion = parseInt 3
 parseTypeId = parseInt 3
@@ -129,6 +140,7 @@ parseTypeId = parseInt 3
 -- Remove all the leading marks from groups to get the original bit string
 -- representing the literal value.
 parseGroups :: Parser [BitString]
+parseGroups [] = error "Expected groups"
 parseGroups (Zero:bs) = PR [first] fRemainder
     where PR first fRemainder = parseGroup bs
 parseGroups (One:bs) = PR (first:more) mRemainder
@@ -136,6 +148,7 @@ parseGroups (One:bs) = PR (first:more) mRemainder
           PR more mRemainder = parseGroups fRemainder
 
 parseGroup :: Parser BitString
+parseGroup [] = error "Expected group"
 parseGroup bs = PR (take 4 bs) (drop 4 bs)
 
 --------------
@@ -175,3 +188,5 @@ example4 = doExample example4hex (Operator (Hdr 4 0) [
             ])
         ])
     ])
+
+main = example2
