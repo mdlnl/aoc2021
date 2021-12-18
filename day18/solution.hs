9b2484fb5ecef9423a2bb49a5d874a2f7f4535a4
Madeline Lein <madeline.lein@gmail.com>
2021-12-18 00:33:47 -0800

    explode is working

--- day18/solution.hs
+++ day18/solution.hs
@@ -1,10 +1,14 @@
 import Data.Maybe
 
-data Snailfish = R Int | P Int Snailfish Snailfish deriving (Eq, Show)
+data Snailfish = R Int
+               | P Int Snailfish Snailfish
+               | X Int -- tracks where an explosion happened, as well as the leftgoing blast
+               deriving (Eq, Show)
 --instance Show Snailfish where
 
 pretty (R r) = show r
 pretty (P _ p1 p2) = "[" ++ pretty p1 ++ "," ++ pretty p2 ++ "]"
+pretty (X a) = show a ++ "!"
 
 toParenthesesMap c
     | c == '['  = '('
@@ -26,23 +30,27 @@ parsePair depth ('[':s) = PR (P depth p1 p2) p2rem
     where PR p1 (',':p1rem) = parseSnailfish (depth + 1) s
           PR p2 (']':p2rem) = parseSnailfish (depth + 1) p1rem
 
-data ExplosionState = Searching | Found Int | Added deriving Show
+data ExplosionState = Searching | Found Int | Done deriving Show
 data ExplosionStep = ExplosionStep ExplosionState Snailfish deriving Show
 
 boom (ExplosionStep _ s) = s
 
-explodeRight Added s = ExplosionStep Added s
-explodeRight (Found n) (R r) = ExplosionStep Added (R $ n + r)
+explodeRight Done s = ExplosionStep Done s
+explodeRight (Found n) (R r) = ExplosionStep Done (R $ n + r)
 explodeRight state (R r) = ExplosionStep state (R r)
-explodeRight Searching (P 4 (R _) (R b)) = ExplosionStep (Found b) (R 0)
+explodeRight Searching (P 4 (R a) (R b)) = ExplosionStep (Found b) (X a)
 explodeRight state (P d a b) = ExplosionStep afterB (P d ar br)
     where ExplosionStep afterA ar = explodeRight state a
           ExplosionStep afterB br = explodeRight afterA b
 
-explodeLeft Added s = ExplosionStep Added s
-explodeLeft (Found n) (R r) = ExplosionStep Added (R $ n + r)
+explodeLeft Done s = ExplosionStep Done s
+explodeLeft (Found n) (R r) = ExplosionStep Done (R $ n + r)
 explodeLeft state (R r) = ExplosionStep state (R r)
-explodeLeft Searching (P 4 (R a) (R _)) = ExplosionStep (Found a) (R 0)
+explodeLeft Searching (X a) = ExplosionStep (Found a) (R 0)
 explodeLeft state (P d a b) = ExplosionStep afterA (P d ar br)
     where ExplosionStep afterB br = explodeLeft state b
           ExplosionStep afterA ar = explodeLeft afterB a
+
+explode sf0 = sf2
+    where ExplosionStep after0 sf1 = explodeRight Searching sf0 -- has to happen first since we want leftmost explodable pair
+          ExplosionStep after1 sf2 = explodeLeft  Searching sf1
