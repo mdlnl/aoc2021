f7a6c6fe4692a4a4c94badfce164718d073ed20e
Madeline Lein <madeline.lein@gmail.com>
2021-12-18 01:20:36 -0800

    split working

--- day18/solution.hs
+++ day18/solution.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Maybe
+import Data.Ratio
 
 data Snailfish = R Int
                | P Int Snailfish Snailfish
@@ -25,7 +26,13 @@ parse string = result
     where PR result [] = parseSnailfish 0 string
 
 parseSnailfish depth s@('[':_) = parsePair depth s
-parseSnailfish depth s@(c:cs) = PR (R (read [c])) cs
+parseSnailfish depth s@(c:cs) = PR (R ival) rem
+    where PR ival rem = parseInt s
+
+parseInt (c:',':cs) = PR (read [c]) (',':cs)
+parseInt (c:']':cs) = PR (read [c]) (']':cs)
+parseInt (c:d:',':cs) = PR (read [c,d]) (',':cs)
+parseInt (c:d:']':cs) = PR (read [c,d]) (']':cs)
 
 parsePair depth ('[':s) = PR (P depth p1 p2) p2rem
     where PR p1 (',':p1rem) = parseSnailfish (depth + 1) s
@@ -59,25 +66,45 @@ explode sf0 = sf2
     where ExplosionStep after0 sf1 = explodeRight Searching sf0 -- has to happen first since we want leftmost explodable pair
           ExplosionStep after1 sf2 = explodeLeft  Searching sf1
 
+explosionTests = do
+    putStrLn $ show $ t "[[[[[9,8],1],2],3],4]"                 "[[[[0,9],2],3],4]"
+    putStrLn $ show $ t "[7,[6,[5,[4,[3,2]]]]]"                 "[7,[6,[5,[7,0]]]]"
+    putStrLn $ show $ t "[[6,[5,[4,[3,2]]]],1]"                 "[[6,[5,[7,0]]],3]"
+    putStrLn $ show $ t "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]" "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+    putStrLn $ show $ t "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"     "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+    where t = test explode "explode"
+
 -----------
 -- Split --
 
+data SplitState = Splitting | Split deriving Show
+data SplitStep = SplitStep SplitState Snailfish deriving Show
 
+crack (SplitStep _ s) = s
+
+split0 = split 0 Splitting
+
+split _ Split s = SplitStep Split s
+split d Splitting (R r)
+    | r >= 10   = SplitStep Split     (P d (R $ floor $ r%2) (R $ ceiling $ r%2))
+    | otherwise = SplitStep Splitting (R r)
+split d state (P _ a b) = SplitStep afterB (P d ar br)
+    where SplitStep afterA ar = split (d + 1) state a
+          SplitStep afterB br = split (d + 1) afterA b
+
+splitTests = do
+    putStrLn $ show $ t "[[[[0,7],4],[15,[0,13]]],[1,1]]"    "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
+    putStrLn $ show $ t "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]" "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"
+    where t = test (crack . split 0 Splitting) "split"
 
 -------------
 -- Testing --
 
 data TestResult = Pass String | Fail String deriving Show
 
-explosionTest input expected
-    | x == e    = Pass $ "explode " ++ input
-    | otherwise = Fail $ "explode " ++ input
-    where x = explode $ parse input
+test :: (Snailfish -> Snailfish) -> String -> String -> String -> TestResult
+test f desc input expected
+    | x == e    = Pass $ desc ++ " " ++ input
+    | otherwise = Fail $ desc ++ " " ++ input
+    where x = f $ parse input
           e = parse $ expected
-
-explosionTests = do
-    putStrLn $ show $ explosionTest "[[[[[9,8],1],2],3],4]"                 "[[[[0,9],2],3],4]"
-    putStrLn $ show $ explosionTest "[7,[6,[5,[4,[3,2]]]]]"                 "[7,[6,[5,[7,0]]]]"
-    putStrLn $ show $ explosionTest "[[6,[5,[4,[3,2]]]],1]"                 "[[6,[5,[7,0]]],3]"
-    putStrLn $ show $ explosionTest "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]" "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
-    putStrLn $ show $ explosionTest "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"     "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
