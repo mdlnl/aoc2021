c3ab0852b3978e3e31a6bec23fe6c0686225b1ba
Madeline Lein <61875345+mdlnl@users.noreply.github.com>
2021-12-19 08:31:26 +0000

    working LCS

--- LCS.hs
+++ LCS.hs
@@ -1,32 +1,36 @@
 module LCS (lcsWith) where
 
-import Data.Map (Map, assocs, fromList, union, insert)
+import Data.Map (Map, assocs)
 import qualified Data.Map as Map
 import Data.Maybe
+import Data.Set (Set, empty)
+import qualified Data.Set as Set
 import Debug.Trace
 
-lcs :: (Show a, Eq a) => [a] -> [a] -> [[a]]
+lcs :: (Ord a, Eq a) => [a] -> [a] -> Set [a]
 lcs = lcsWith (==)
 
 -- lcsWith eq x y returns the longest common subsequnces of x and y, where equality
 -- is determined by eq
-lcsWith :: Show a => (a -> a -> Bool) -> [a] -> [a] -> [[a]]
-lcsWith _ [] _ = []
-lcsWith _ _ [] = []
+lcsWith :: (Ord a) => (a -> a -> Bool) -> [a] -> [a] -> Set [a]
+lcsWith _ [] _ = empty
+lcsWith _ _ [] = empty
 lcsWith eq x y = ij m n t
     where m = length x
           n = length y
           t = lcsWithDp (initial m n) 1 1 eq x y
 
-type Table a = Map (Int, Int) [[a]]
+type Table a = Map (Int, Int) (Set [a])
 
 ij i j t = fromJust $ Map.lookup (i, j) t
 
-initial m n = union topRow leftCol
-    where topRow = fromList [ ((0, j), [[]]) | j <- [0..m] ]
-          leftCol = fromList [ ((i, 0), [[]]) | i <- [0..m] ]
+emptyPrefix = Set.singleton []
 
-lcsWithDp :: Show a => Table a -> Int -> Int -> (a -> a -> Bool) -> [a] -> [a] -> Table a
+initial m n = Map.union topRow leftCol
+    where topRow  = Map.fromList [ ((0, j), emptyPrefix) | j <- [0..m] ]
+          leftCol = Map.fromList [ ((i, 0), emptyPrefix) | i <- [0..m] ]
+
+lcsWithDp :: (Ord a) => Table a -> Int -> Int -> (a -> a -> Bool) -> [a] -> [a] -> Table a
 lcsWithDp dp i j eq x y
     | i > m     = dp
     | j > n     = lcsWithDp dp (i+1) 1 eq x y
@@ -34,22 +38,21 @@ lcsWithDp dp i j eq x y
     where m = length x
           n = length y
           lcsij = compute dp i j eq x y
-          nextdp = insert (i, j) lcsij dp
+          nextdp = Map.insert (i, j) lcsij dp
 
-compute :: Show a => Table a -> Int -> Int -> (a -> a -> Bool) -> [a] -> [a] -> [[a]]
+compute :: (Ord a) => Table a -> Int -> Int -> (a -> a -> Bool) -> [a] -> [a] -> Set [a]
 compute dp i j eq x y
-    | eq xi yj  = ij (i-1) (j-1) dp
-    | otherwise = --trace (show (assocs dp) ++ " " ++ show i ++ " " ++ show j) $
-                  keepLongest above left
+    | eq xi yj  = Set.map (++[xi]) $ ij (i-1) (j-1) dp
+    | otherwise = keepLongest above left
     where xi = x !! (i - 1)
           yj = y !! (j - 1)
           above = ij (i-1) j dp
           left  = ij i (j-1) dp
 
-keepLongest :: [[a]] -> [[a]] -> [[a]]
-keepLongest alist@(a:as) blist@(b:bs)
-    | alen > blen = a : as
-    | blen > alen = b : bs
-    | otherwise   = alist ++ blist
-    where alen = length a
-          blen = length b
+keepLongest :: Ord a => Set [a] -> Set [a] -> Set [a]
+keepLongest as bs
+    | alen > blen = as
+    | blen > alen = bs
+    | otherwise   = Set.union as bs
+    where alen = length $ (Set.toList as) !! 0
+          blen = length $ (Set.toList bs) !! 0
