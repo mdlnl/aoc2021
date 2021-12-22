f283757b7c56a7c4301c35567273d75d02e20fc5
Madeline Lein <61875345+mdlnl@users.noreply.github.com>
2021-12-22 00:02:24 +0000

    parsing (no map generation yet)

--- day20/solution.hs
+++ day20/solution.hs
@@ -1,14 +1,31 @@
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Debug.Trace
+import Split
 
 data Pixel = Dark | Light deriving Eq
 instance Show Pixel where
     show Dark = "."
     show Light = "#"
 
-type Image = Map (Int, Int) -> Pixel
+toPixel '.' = Dark
+toPixel '#' = Light
 
+type Image = Map (Int, Int) Pixel
+
+pixelAt :: (Int, Int) -> Image -> Pixel
 pixelAt = Map.findWithDefault Dark
 
-countLight = length $ filter (= Light) $ Map.elems
+countLight = length . (filter (== Light)) . Map.elems
 
+parsePixels = map toPixel
+
+parseImage lines = 
+    where rows = map parsePixels
+
+parseInput input = (parsePixels algoLine, parseImage imageLines)
+    where algoLine:"":imageLines = nlsplit input
+
+parseFile filename = do
+    input <- readFile filename
+    return $ parseInput input
