d8bb248487471ec15525e99c384bcbd1c1d90ff9
Madeline Lein <madeline.lein@gmail.com>
2021-12-24 18:49:39 -0800

    step/run compile

--- day24/solution.hs
+++ day24/solution.hs
@@ -7,23 +7,34 @@ data Variable = W | X | Y | Z deriving (Eq, Ord)
 
 data Arg = Var Variable | Lit Integer | Empty
 
-data Instruction = Inp Variable Arg
-                 | Add Variable Arg
-                 | Mul Variable Arg
-                 | Div Variable Arg
-                 | Mod Variable Arg
-                 | Eql Variable Arg
+data Op = Ins | Add | Mul | Div | Mod | Eql
 
-op (Add _ _) = (+)
-op (Mul _ _) = (*)
-op (Div _ _) = div
-op (Mod _ _) = mod
-op (Eql _ _) = \a b -> if a == b then 1 else 0
+data Instruction = Inst { operator::Op, left::Variable, right::Arg }
+
+op :: Op -> (Integer -> Integer -> Integer)
+op Add = (+)
+op Mul = (*)
+op Div = div
+op Mod = mod
+op Eql = \a b -> if a == b then 1 else 0
 
 type Memory = Map Variable Integer
 
 initialMemory = fromList [(W, 0), (X, 0), (Y, 0), (Z, 0)]
 
-get :: Memory -> Arg -> Integer
-get m (Var v) = fromJust $ Map.lookup v m
-get _ (Lit x) = x
+get :: Arg -> Memory -> Integer
+get (Var v) m = fromJust $ Map.lookup v m
+get (Lit x) _ = x
+
+type State = (Memory, [Integer])
+
+step :: State -> Instruction -> State
+step (mem, inp:inps) (Inst Ins var _) = (insert var inp mem,      inps)
+step (mem, inputs)   instruction      = (insert dest (o a b) mem, inputs) 
+    where dest = left instruction
+          a = fromJust $ Map.lookup dest mem
+          b = get (right instruction) mem
+          o = op $ operator instruction
+
+run :: [Integer] -> [Instruction] -> Memory
+run inputs = fst . foldl step (initialMemory, inputs)
